6c463e24fe00b659777def76f0decaf2165e9b4f
Jose Eduardo O. T <rotjeot@gmail.com>
2020-04-13 23:10:41 -0500

    Creacion del repositorio para el proyecto de tiro parabolico.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Tiro_parabolico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Play = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(12, 12);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(103, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(580, 284);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(12, 56);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 2;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(93, 359);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(590, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(752, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(507, 284);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1303, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//*******************************
+//bibliotecas de EmguCV
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+//*******************************
+
+namespace Tiro_parabolico
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture video;
+        bool IsPlaying =false;
+        int TotalFrames;
+        int CurrentFrameNo;
+        Mat CurrentFrame;
+        int FPS;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void load_Click(object sender, EventArgs e)
+            //Boton para load para cargar archivos
+        {
+            Buscar();
+        }
+
+
+        private void Buscar()//Buscar fotos en el explorador de windows
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            string ruta;
+
+            try
+            {
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    ruta = openFileDialog1.FileName;
+                    video = new VideoCapture(ruta);
+                    TotalFrames = Convert.ToInt32(video.GetCaptureProperty(CapProp.FrameCount));
+                    FPS = Convert.ToInt32(video.GetCaptureProperty(CapProp.Fps));
+                    IsPlaying = true;
+                    CurrentFrame = new Mat();
+                    CurrentFrameNo = 0;
+                    trackBar1.Minimum = 0;
+                    trackBar1.Maximum = TotalFrames - 1;
+                    trackBar1.Value = 0;
+                    PlayVideo();
+
+                    
+                   
+                    //matrix.Data[]
+                    
+                    
+                }
+            }
+            catch
+            {
+            }
+
+
+        }
+
+        private async void PlayVideo()
+        {
+           if(video == null)
+            {
+                return;
+            }
+            try
+            {
+                while(IsPlaying == true && CurrentFrameNo < TotalFrames)
+                {
+                    video.SetCaptureProperty(CapProp.PosFrames, CurrentFrameNo);
+                    video.Read(CurrentFrame);
+                    pictureBox1.Image = CurrentFrame.Bitmap;
+                    trackBar1.Value = CurrentFrameNo;
+                    CurrentFrameNo += 1;
+                    await Task.Delay(1000 / FPS);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            if(video != null)
+            {
+                IsPlaying = true;
+                PlayVideo();
+            }
+            else
+            {
+                IsPlaying = false;
+            }
+            
+           
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+           if(video != null)
+            {
+                CurrentFrameNo = trackBar1.Value;
+            }
+
+        }
+    }
+}
